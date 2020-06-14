9d1772b65fb7a781ffb255b6535f85a3e0d1ed6d
Cyril Wanner <info@cyr.li>
2020-06-14 18:35:06 +0200

    Add typescript declaration file for images

---
+++ images.d.ts
@@ -0,0 +1,75 @@
+export interface ImgSrc {
+  src: string;
+  width: number;
+  height: number;
+  format: string;
+  toString(): string;
+}
+
+export interface ColorsSrc {
+  src: string[];
+  width: number;
+  height: number;
+  format: string;
+  toString(): string;
+}
+
+declare module '*.png' {
+  const value: ImgSrc;
+  export = value;
+}
+
+declare module '*.png?colors' {
+  const value: ColorsSrc;
+  export = value;
+}
+
+declare module '*.jpg' {
+  const value: ImgSrc;
+  export = value;
+}
+
+declare module '*.jpg?colors' {
+  const value: ColorsSrc;
+  export = value;
+}
+
+declare module '*.jpeg' {
+  const value: ImgSrc;
+  export = value;
+}
+
+declare module '*.jpeg?colors' {
+  const value: ColorsSrc;
+  export = value;
+}
+
+declare module '*.webp' {
+  const value: ImgSrc;
+  export = value;
+}
+
+declare module '*.webp?colors' {
+  const value: ColorsSrc;
+  export = value;
+}
+
+declare module '*.svg' {
+  const value: ImgSrc;
+  export = value;
+}
+
+declare module '*.svg?colors' {
+  const value: ColorsSrc;
+  export = value;
+}
+
+declare module '*.gif' {
+  const value: ImgSrc;
+  export = value;
+}
+
+declare module '*.gif?colors' {
+  const value: ColorsSrc;
+  export = value;
+}
