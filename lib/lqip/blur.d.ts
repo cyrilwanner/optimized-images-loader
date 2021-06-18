bc04fe3a5d588728898f197aff6667969e2ed152
Clement Balestrat <clement.balestrat@gmail.com>
2021-06-18 10:06:01 +1000

    lib folder removed

--- lib/lqip/blur.d.ts
+++
@@ -1,6 +0,0 @@
-import { ImageOptions } from '../parseQuery';
-declare const calculateBlurOptions: (imageInfo: {
-    width?: number;
-    height?: number;
-}, imageOptions: ImageOptions) => void;
-export default calculateBlurOptions;
--- lib/util.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Returns the version of optimized-images-loader
- *
- * @async
- * @returns {string} Package version
- */
-export declare const getLoaderVersion: () => Promise<string>;
